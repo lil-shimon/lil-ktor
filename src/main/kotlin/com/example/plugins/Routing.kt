9a14321ab051baa2da497c4fc6776ae76aa159be
Kenta Shimosawa <shimon101018@gmail.com>
2022-08-12 02:38:42 +0900

    feat: 🎸 inject repository into koin

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
@@ -7,5 +8,6 @@ import io.ktor.server.application.*
 fun Application.configureRouting() {
     routing {
         root()
+        getAllHeroes()
     }
 }
