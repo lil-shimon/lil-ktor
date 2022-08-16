ccf0cb8110e8843b83a84d3031e325f5998a9c11
Kenta Shimosawa <shimon101018@gmail.com>
2022-08-17 00:13:35 +0900

    feat: 🎸 add image route and rename image file

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,10 +4,18 @@ import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        // Static content path
+        // images dir on resources folder (src/main/resources/images)
+        // can access from http://localhost:8080/images/{image name}
+        static("/images") {
+            resources("images")
+        }
     }
 }
